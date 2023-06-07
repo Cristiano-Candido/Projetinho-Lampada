102a79bf73f841c31d104114c97f47e72c2b035e
Khristiano <cristiano.objetiva.lg@gmail.com>
2023-06-07 09:14:41 -0300

    Projeto Lâmpada

---
+++ script.js
@@ -0,0 +1,9 @@
+function acendeLampada(){
+    document.getElementById('myImage').src = "/PRINCIPAL/projeto-lampada/imagem/pic_bulbon.jpg"; 
+  }
+  function apagaLampada(){
+    document.getElementById('myImage').src = "/PRINCIPAL/projeto-lampada/imagem/pic_bulboff.jpg";
+  }
+  function lampadaQuebrada(){
+    document.getElementById('myImage').src = "/PRINCIPAL/projeto-lampada/imagem/broken_lamp.jpg";
+  }
